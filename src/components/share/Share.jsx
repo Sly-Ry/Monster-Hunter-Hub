81a9901cb4cc267ad903498caa4b8b2ab090467a
ryan <ryan.williams93@aol.com>
2022-07-31 13:37:06 -0500

    share feed set up

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,25 @@
+import "./share.css"
+import { PermMedia } from "@material-ui/icons"
+
+export default function Share() {
+  return (
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img className="shareProfileImg" src="/assets/person/1.jpg" alt="" />
+                <input 
+                    placeholder="What's on your mind?" className="shareInput"/>
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="shareOption">
+                        <PermMedia className="shareIcon"/>
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
