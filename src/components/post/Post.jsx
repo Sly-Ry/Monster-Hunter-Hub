fee4b0418f60297730a3037e14210e918b3eeeed
ryan <ryan.williams93@aol.com>
2022-08-06 15:45:06 -0500

    top of post set up: profileimg, username, styling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,26 @@
 import "./post.css"
+import { MoreVert } from "@material-ui/icons"
 
 export default function Post() {
   return (
-    <div>
-        Post
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTLeft">
+                    <img className="postProfileImg" src="/assets/person/1.jpg" alt="" />
+                    <span className="postUsername">Ben Dova</span>
+                    <span className="postDate">5 mins ago</span>
+                </div>
+                <div className="postTRight">
+                    <MoreVert />
+                </div>
+            </div>
+            <div className="postCenter">
+                <div className="postContainer"></div>
+            </div>
+            <div className="postBottom"></div>
+        </div>
+
     </div>
   )
 }
