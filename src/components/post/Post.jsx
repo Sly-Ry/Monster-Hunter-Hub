f658391810774764cb3c513237f09521162bee7a
ryan <ryan.williams93@aol.com>
2022-08-14 17:21:07 -0500

    generates user data dynamically from mockdata

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,15 @@
 import "./post.css"
 import { MoreVert } from "@material-ui/icons"
+import { Users } from "../../mockData"
 
 export default function Post({post}) {
-    console.log(post)
     return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTLeft">
-                    <img className="postProfileImg" src="/assets/person/1.jpg" alt="" />
-                    <span className="postUsername">Ben Dova</span>
+                    <img className="postProfileImg" src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" />
+                    <span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
                     <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTRight">
