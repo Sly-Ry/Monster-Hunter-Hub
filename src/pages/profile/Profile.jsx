48232e6b534172244d900a1ce793ba67b08aa41a
ryan <ryan.williams93@aol.com>
2022-08-17 14:43:53 -0500

    profile page set up, cover and user image styling

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import "./profile.css"
+import Navbar from "../../components/nav/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+
+export default function Profile() {
+  return (
+    <>
+      <Navbar/>
+      <div className="profile">
+        <Sidebar/>
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img className="profileCoverImg" src="assets/post/m3.jpg" alt="" />
+              <img src="assets/person/3.jpg" alt="" className="profileUserImg" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileName">Ryan W</h4>
+              <span className="profileInfoDesc">
+                Sup homies
+              </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed/>
+            <Rightbar/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
