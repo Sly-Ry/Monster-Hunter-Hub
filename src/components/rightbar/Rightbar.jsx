f1e612258e497fdc4256b32dad1c686b09b9be33
ryan <ryan.williams93@aol.com>
2022-08-17 14:57:24 -0500

    added user info to profile page

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,11 @@ import "./rightbar.css"
 import { Users } from "../../mockData"
 import Online from "../online/Online";
 
-export default function Rightbar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+    
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="rightbarContainer">
                     <img src="assets/birthday.png" alt="" className="birthdayImg" />
                     <span className="birthdayText">
@@ -19,6 +20,36 @@ export default function Rightbar() {
                         <Online key={u.id} user={u}/>
                     ))}
                 </ul>
+            </>
+        );
+    }
+
+    const ProfileRightBar = () => {
+        return (
+            <>
+            <h4 className="rightBarTitle">User Info</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">From: </span>
+                    <span className="rightbarInfoValue">Mexico City</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">City: </span>
+                    <span className="rightbarInfoValue">New York</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">Relationship: </span>
+                    <span className="rightbarInfoValue">Single</span>
+                </div>
+            </div>
+            </>
+        );
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <ProfileRightBar />
             </div>
         </div>
     ); 
