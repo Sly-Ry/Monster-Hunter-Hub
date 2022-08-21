bbd0dc56192f65ebc8904655995e550dc1664447
ryan <ryan.williams93@aol.com>
2022-08-21 16:06:12 -0500

    login file initialized and login page created

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import'./login.css'
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">MHH</h3>
+                    <span className="loginDesc">
+                        Hunting together is fun!
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
